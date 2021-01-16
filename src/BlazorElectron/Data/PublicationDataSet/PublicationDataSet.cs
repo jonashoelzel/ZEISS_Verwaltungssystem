5e6da2063367787393b40bfcd4bf10a6755d3b3e
Stephan Joneleit <StephanJoneleit@outlook.de>
2021-01-16 17:23:33 +0100

    Added PublicationDataSet

---
+++ src/BlazorElectron/Data/PublicationDataSet/PublicationDataSet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorElectron.Data.PublicationDataSet
+{
+    public class PublicationDataSet
+    {
+        // Publication Information
+        private int? _PublicationID;
+        public int? PublicationID
+        {
+            get { return _PublicationID; }
+            set
+            {
+                if (_PublicationID is null)
+                    _PublicationID = PublicationID;
+            }
+        }
+        public string WorkingTitle { get; set; }
+        public string PublicationTitle { get; set; }
+        //Type of medium the medium will be published (newspaper, magazin, book, blog, [...])
+        public string PublicationType { get; set; }
+
+
+        // Additional Information
+        public string CurrentState { get; set; }
+        public DateTime DateOfStartWorking { get; set; }
+        public DateTime DateOfRelease { get; set; }
+        public List<string> Tags { get; set; }
+        public string Description { get; set; }
+        public string AdditionalInformation { get; set; }
+
+
+        // Author Information
+        public Author MainAuthor = new Author();
+        public List<Author> CoAuthors { get; set; }
+
+
+        // Publisher Information
+        public Publisher PublishedBy = new Publisher();
+    }
+
+
+    public class Author
+    {
+        public int? _ID;
+        public int? ID
+        {
+            get { return _ID; }
+            set
+            {
+                if (_ID is null)
+                    _ID = ID;
+            }
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        //What division is exatctly meant?!
+        public string Division { get; set; }
+    }
+
+
+    public class Publisher
+    {
+        private int? _ID;
+        public int? ID
+        {
+            get { return _ID; }
+            set
+            {
+                if (_ID is null)
+                    _ID = ID;
+            }
+        }
+        public string Name { get; set; }
+    }
+}
