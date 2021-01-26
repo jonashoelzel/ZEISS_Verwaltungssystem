95dbb297321f0c6c8ad169410b4813f0d49ca4f3
Jonas Hölzel <jonas.hoelzel@htw-dresden.de>
2021-01-26 23:30:57 +0100

    Add Logic

---
+++ src/BlazorElectron/Data/DataLogic/Logic.cs
@@ -0,0 +1,22 @@
+﻿using Data.DataSet;
+using Data.ExcelIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorElectron.Data.DataLogic
+{
+    public class Logic
+    {
+        public static void Save(IPublicationDataSet dataSet)
+        {
+            // var excelIO = new WriteDataSet("filePaht", "sheetName");
+            // excelIO.Insert(dataSet);
+
+
+
+            WriteDataSet.Insert("ExcelDataBase.xlsx", "Publication", dataSet);
+        }
+    }
+}
