567d025a96bbe40f7ba665871ec94e95f2b16742
YeXtaiZ <StephanJoneleit@outlook.de>
2021-04-18 09:57:53 +0200

    Update: Refactored WriteDataSet.

--- src/DataSetIO/WriteDataSet.cs
+++ src/DataSetIO/WriteDataSet.cs
@@ -36,92 +36,13 @@ namespace Zeiss.PublicationManager.Data.DataSet.IO.Write
         public static void Insert(string filepath, string worksheetName, IPublicationDataSet dataSet)
         {
             InitializeDataSetWorksheet(filepath, worksheetName);
-
-            List<object> entry = new List<object>()
-            {
-                dataSet.ID,
-                dataSet.WorkingTitle,
-                dataSet.PublicationTitle,
-
-                dataSet.TypeOfPublication.Name,
-
-                dataSet.MainAuthor.ID,
-                dataSet.MainAuthor.Name,
-                dataSet.MainAuthor.Surname,
-                ConvertCoAuthorsToCSV(dataSet.CoAuthors),
-                dataSet.Division,
-
-                dataSet.DateOfStartWorking.Year,
-                dataSet.CurrentState,
-                dataSet.DateOfRelease,
-
-                dataSet.PublishedBy.ID,
-                dataSet.PublishedBy.Name,
-
-                ConvertTagsToCSV(dataSet.Tags),
-                dataSet.Description,
-                dataSet.AdditionalInformation,
-            };
-
-            ExcelInsert.Insert(filepath, worksheetName, entry);
+            ExcelInsert.Insert(filepath, worksheetName, GetNewRow(dataSet));
         }
 
         public static void InsertIntelligent(string filepath, string worksheetName, IPublicationDataSet dataSet)
         {
-            InitializeDataSetWorksheet(filepath, worksheetName);
-
-            List<string> columns = new()
-            {
-                "Publikations-ID",
-                "Arbeitstitel",
-                "Veröffentlichungstitel",
-                "Veröffentlichungsmedium",
-
-                "Autor-ID",
-                "Vorname",
-                "Nachname",
-                "Co-Autoren",
-                "Division",
-
-                "Arbeitsbeginn (Startjahr)",
-                "Derzeitiger Arbeitsstatus",
-                "Veröffentlichungsdatum",
-
-                "Publisher-ID",
-                "Publisher",
-
-                "Tags",
-                "Beschreibung (zusätzlich)",
-                "Zusätzliche Informationen",
-            };
-
-            List<object> entry = new List<object>()
-            {
-                dataSet.ID,
-                dataSet.WorkingTitle,
-                dataSet.PublicationTitle,
-
-                dataSet.TypeOfPublication.Name,
-
-                dataSet.MainAuthor.ID,
-                dataSet.MainAuthor.Name,
-                dataSet.MainAuthor.Surname,
-                ConvertCoAuthorsToCSV(dataSet.CoAuthors),
-                dataSet.Division,
-
-                dataSet.DateOfStartWorking.Year,
-                dataSet.CurrentState,
-                dataSet.DateOfRelease,
-
-                dataSet.PublishedBy.ID,
-                dataSet.PublishedBy.Name,
-
-                ConvertTagsToCSV(dataSet.Tags),
-                dataSet.Description,
-                dataSet.AdditionalInformation,
-            };
-
-            ExcelInsert.Insert(filepath, worksheetName, columns, entry);
+            InitializeDataSetWorksheet(filepath, worksheetName);                
+            ExcelInsert.Insert(filepath, worksheetName, GetColumnNames().Select(x => x.ToString()).ToList(), GetNewRow(dataSet));
         }
 
 
@@ -199,35 +120,66 @@ namespace Zeiss.PublicationManager.Data.DataSet.IO.Write
         private static void InitializeDataSetWorksheet(string filepath, string worksheetName)
         {
             if (!WriteExcel.WorksheetExists(ref filepath, worksheetName))
-            {
-                List<object> entry = new List<object>()
-                {
-                    "Publikations-ID",
-                    "Arbeitstitel",
-                    "Veröffentlichungstitel",
-                    "Veröffentlichungsmedium",
-
-                    "Autor-ID",
-                    "Vorname",
-                    "Nachname",
-                    "Co-Autoren",
-                    "Division",
-
-                    "Arbeitsbeginn (Startjahr)",
-                    "Derzeitiger Arbeitsstatus",
-                    "Veröffentlichungsdatum",
-
-                    "Publisher-ID",
-                    "Publisher",
-
-                    "Tags",
-                    "Beschreibung (zusätzlich)",
-                    "Zusätzliche Informationen",
-                };
-
-                ExcelInsert.Insert(filepath, worksheetName, entry);
+            {              
+                ExcelInsert.Insert(filepath, worksheetName, GetColumnNames());
             }
         }
 
+        public static List<object> GetColumnNames()
+        {
+            return new List<object>()
+            {
+                "Publikations-ID",
+                "Arbeitstitel",
+                "Veröffentlichungstitel",
+                "Veröffentlichungsmedium",
+
+                "Autor-ID",
+                "Vorname",
+                "Nachname",
+                "Co-Autoren",
+                "Division",
+
+                "Arbeitsbeginn (Startjahr)",
+                "Derzeitiger Arbeitsstatus",
+                "Veröffentlichungsdatum",
+
+                "Publisher-ID",
+                "Publisher",
+
+                "Tags",
+                "Beschreibung (zusätzlich)",
+                "Zusätzliche Informationen",
+            };
+        }
+
+        public static List<object> GetNewRow(IPublicationDataSet dataSet)
+        {
+            return new List<object>()
+            {
+                dataSet.ID,
+                dataSet.WorkingTitle,
+                dataSet.PublicationTitle,
+
+                dataSet.TypeOfPublication.Name,
+
+                dataSet.MainAuthor.ID,
+                dataSet.MainAuthor.Name,
+                dataSet.MainAuthor.Surname,
+                ConvertCoAuthorsToCSV(dataSet.CoAuthors),
+                dataSet.Division,
+
+                dataSet.DateOfStartWorking.Year,
+                dataSet.CurrentState,
+                dataSet.DateOfRelease,
+
+                dataSet.PublishedBy.ID,
+                dataSet.PublishedBy.Name,
+
+                ConvertTagsToCSV(dataSet.Tags),
+                dataSet.Description,
+                dataSet.AdditionalInformation,
+            };
+        }
     }
 }
